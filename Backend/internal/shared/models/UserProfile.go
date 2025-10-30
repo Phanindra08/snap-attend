b43868e01ce63d5877cd23ded3404f69ed2384c2
Sreeya Nallamolu <sreeyanallamolu@gmail.com>
2025-10-30 16:11:01 -0400

    Creating UserProfile model for creating user_profile table, storing and retrieving user_profile details

---
+++ Backend/internal/shared/models/UserProfile.go
@@ -0,0 +1,14 @@
+package models
+
+type userRoles string
+
+const (
+	student   userRoles = "Student"
+	professor userRoles = "Professor"
+	admin     userRoles = "Admin"
+)
+
+type UserProfile struct {
+	ProfileId uint      `gorm:"primaryKey; autoIncrement"`
+	Role      userRoles `gorm:"unique; not null"`
+}
