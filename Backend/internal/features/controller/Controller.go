207fe99c37965648964ca95045995346b808ac5c
Phanindra08 <kothuriphanindra1999@gmail.com>
2025-11-21 01:28:37 -0500

    Creating a Controller to handle all the controller, services and repo instances

---
+++ Backend/internal/features/controller/Controller.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"sync"
+
+	"github.com/phanindra08/snap-attend/internal/features/repository"
+	"github.com/phanindra08/snap-attend/internal/features/service"
+	"github.com/phanindra08/snap-attend/internal/shared/database"
+)
+
+type Controller struct {
+	User *UserController
+}
+
+var (
+	controllersOnce    sync.Once
+	controllerInstance *Controller
+)
+
+// GetControllers returns a singleton instance holding all controller pointers.
+func GetControllers() *Controller {
+	controllersOnce.Do(func() {
+		db := database.GetDB()
+
+		// Create all the repositories
+		userRepo := repository.NewUserRepository(db)
+
+		// Create all the services
+		userService := service.NewUserService(userRepo)
+
+		// Create all the controllers
+		controllerInstance = &Controller{
+			User: NewUserController(userService),
+		}
+	})
+	return controllerInstance
+}
