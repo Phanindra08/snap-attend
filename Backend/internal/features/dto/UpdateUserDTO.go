0cdda9f49d253d947d1431a489aeee68f8068ec9
Phanindra08 <kothuriphanindra1999@gmail.com>
2025-11-21 00:21:06 -0500

    Updating UpdateProfileRequest and UpdateUserDTO structs based on inputs requests

--- Backend/internal/features/dto/UpdateUserDTO.go
+++ Backend/internal/features/dto/UpdateUserDTO.go
@@ -1,7 +1,15 @@
 package dto
 
 type UpdateUserDTO struct {
-	FirstName string
-	LastName  string
-	Password  string
+	FirstName   string
+	LastName    string
+	OldPassword string
+	NewPassword string
+
+	Address1 string
+	Address2 string
+	City     string
+	State    string
+	ZipCode  string
+	Country  string
 }
